8241153488b905fac14d844ac20ce2fcec173ae0
Replit user <>
2023-05-24 18:50:30 +0000

    Install and Require Helmet

--- myApp.js
+++ myApp.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
 
@@ -52,7 +53,7 @@ const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
-app.get("/", function (request, response) {
+app.get("/", function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
